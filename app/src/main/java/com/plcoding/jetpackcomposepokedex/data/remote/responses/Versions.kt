2f7395af4a5a2b284ea2b3df0563fe2e6eb1b874
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-06-27 16:38:17 -0300

    extra files ignored on first commit

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/Versions.kt
@@ -0,0 +1,11 @@
+package com.plcoding.jetpackcomposepokedex.data.remote.responses
+
+data class Versions(
+    val generation_i: GenerationI,
+    val generation_ii: GenerationIi,
+    val generation_iii: GenerationIii,
+    val generation_iv: GenerationIv,
+    val generation_v: GenerationV,
+    val generation_vi: GenerationVi,
+    val generation_vii: GenerationVii,
+)
